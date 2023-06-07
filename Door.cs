83754336225e536378819090a9c6898be26c0c73
Timofey Kondratiev <kotim4444@gmail.com>
2023-06-07 17:12:34 +0700

    Update Door.cs

--- Door.cs
+++ Door.cs
@@ -1,22 +1,34 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class MoveThief : MonoBehaviour
 {
-    [SerializeField] private Siren siren;
+    [SerializeField] private float _speed;
+
+    private void OnValidate()
+    {
+        if(_speed < 0f) 
+            _speed = 2f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-_speed * Time.fixedDeltaTime, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(_speed * Time.fixedDeltaTime, 0f, 0f);
+        }
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent(out MoveThief thief))
-        {
-            siren.OnPlayerSiren();
+        if (other.TryGetComponent(out Door door)){
+
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.TryGetComponent(out MoveThief thief))
-        {
-            siren.OnStopSiren();
-        }
-    }
 }
+
