18fe96e660d99ea8599c660f4bb42d28691a4964
Timofey Kondratiev <kotim4444@gmail.com>
2023-06-07 17:14:42 +0700

    Update MoveThief.cs

--- MoveThief.cs
+++ MoveThief.cs
@@ -4,27 +4,21 @@ public class MoveThief : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    private Transform _thiefTransform;
-
     private void OnValidate()
     {
-        if(_speed < 0f) _speed = 0f;
-    }
-
-    private void OnEnable()
-    {
-        _thiefTransform = GetComponent<Transform>();
+        if(_speed < 0f) 
+            _speed = 2f;
     }
 
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            _thiefTransform.Translate(-_speed * Time.fixedDeltaTime, 0f, 0f);
+            transform.Translate(-_speed * Time.fixedDeltaTime, 0f, 0f);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            _thiefTransform.Translate(_speed * Time.fixedDeltaTime, 0f, 0f);
+            transform.Translate(_speed * Time.fixedDeltaTime, 0f, 0f);
         }
 
     }
