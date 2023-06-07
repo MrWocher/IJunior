24527f1a18989d6271d3f3ac9bafa6535d9d4b26
Timofey Kondratiev <kotim4444@gmail.com>
2023-06-07 15:32:00 +0700

    Update MoveThief.cs

--- MoveThief.cs
+++ MoveThief.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveThief : MonoBehaviour
 {
-
     [SerializeField] private float _speed;
 
     private Transform _thiefTransform;
@@ -16,27 +13,28 @@ public class MoveThief : MonoBehaviour
 
     private void OnEnable()
     {
-        
         _thiefTransform = GetComponent<Transform>();
-
     }
 
     private void FixedUpdate()
     {
-
         if (Input.GetKey(KeyCode.W))
         {
-
             _thiefTransform.Translate(-_speed * Time.fixedDeltaTime, 0f, 0f);
-
         }
         else if (Input.GetKey(KeyCode.S))
         {
-
             _thiefTransform.Translate(_speed * Time.fixedDeltaTime, 0f, 0f);
-
         }
 
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Door door)){
+
+        }
+    }
+
 }
+
